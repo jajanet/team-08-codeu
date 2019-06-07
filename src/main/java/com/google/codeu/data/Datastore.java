7e7e9a484b1e74808e09621c81ae9c3ca7f6305a
Tyler Hou <tylerhou@google.com>
2019-06-06 20:04:01 -0700

    Add react stats

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -19,15 +19,16 @@ package com.google.codeu.data;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 /** Provides access to the data stored in Datastore. */
 public class Datastore {
@@ -51,24 +52,24 @@ public class Datastore {
   /**
    * Get List of messages posted by a specific user.
    *
-   * @return a list of messages posted by the user, or empty list if user has never posted a
-   *     message. List is sorted by time descending.
+   * @return a list of messages posted by the user, or empty list if user has
+   *     never posted a message. List is sorted by time descending.
    */
   public List<Message> getMessages(String user) {
     List<Message> messages = new ArrayList<>();
 
-    Query query =
-        new Query("Message")
-            .setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user))
-            .addSort("timestamp", SortDirection.DESCENDING);
+    Query query = new Query("Message")
+                      .setFilter(new Query.FilterPredicate(
+                          "user", FilterOperator.EQUAL, user))
+                      .addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
       try {
         String idString = entity.getKey().getName();
         UUID id = UUID.fromString(idString);
-        String text = (String) entity.getProperty("text");
-        long timestamp = (long) entity.getProperty("timestamp");
+        String text = (String)entity.getProperty("text");
+        long timestamp = (long)entity.getProperty("timestamp");
 
         Message message = new Message(id, user, text, timestamp);
         messages.add(message);
@@ -83,17 +84,26 @@ public class Datastore {
   }
 
   /**
-   * Gather a list of all users that have entered something into the community chat. 
+   * Gather a list of all users that have entered something into the community
+   * chat.
    *
-   * @return a Set of users, no order to this list, empty list if there are no users that have posted
+   * @return a Set of users, no order to this list, empty list if there are no
+   *     users that have posted
    */
   public Set<String> getUsers() {
     Set<String> users = new HashSet<>();
     Query query = new Query("Message");
     PreparedQuery results = datastore.prepare(query);
     for (Entity entity : results.asIterable()) {
-      users.add((String) entity.getProperty("user"));
+      users.add((String)entity.getProperty("user"));
     }
     return users;
+  }
+
+  /** Returns the total number of messages for all users. */
+  public int getTotalMessageCount() {
+    Query query = new Query("Message");
+    PreparedQuery results = datastore.prepare(query);
+    return results.countEntities(FetchOptions.Builder.withLimit(1000));
   }
 }
