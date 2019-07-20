d0f115c033cac3015a0df6aa96b684eb4697035e
Janet Vu <vjanet@umich.edu>
2019-07-20 00:42:05 -0400

    Fix compile errors in PlaceTags.java

--- src/main/java/com/google/codeu/data/PlaceTag.java
+++ src/main/java/com/google/codeu/data/PlaceTag.java
@@ -15,102 +15,94 @@
  */
 
 package com.google.codeu.data;
+
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-
 
 /** Class that maps tag-place relationships. */
 public class PlaceTag {
 
-    private Key place;
-    private Key tag;
+  private Key place;
+  private Key tag;
 
-    /**
-     * Constructs a new {@link PlaceTag} posted using only references to a tag and place
-     */
-    public PlaceTag(Key place, Key tag) {
-        this(place);
-        this(tag);
+  /**
+   * Constructs a new {@link PlaceTag} posted using only references to a tag and
+   * place
+   */
+  public PlaceTag(Key place, Key tag) {
+    this.place = place;
+    this.tag = tag;
+  }
+
+  /**
+   * Constructs a new {@link PlaceTag} using a place and the tag
+   */
+  public PlaceTag(Place place, Tag tag) {
+    this.place = KeyFactory.createKey("Place", place.getId().toString());
+    this.tag = KeyFactory.createKey("Tag", tag.getLabel());
+    // would PlaceTag(place.getId(), tag.getLabel()) work too,
+    // as in using the constructor above?
+  }
+
+  /** Return PlaceTag data using based on entity from search query. */
+  public PlaceTag(Entity entity) {
+    // create key based on the string returned from getting the place and tag
+    this.place = KeyFactory.createKey("Place", entity.getProperty("place").toString());
+    this.tag = KeyFactory.createKey("Tag", entity.getProperty("tag").toString());
+  }
+
+  /** Stores a new PlaceTag in Datastore. */
+  public static void store(PlaceTag placeTag) {
+    DatastoreService datastore = Datastore.GetSingletonService();
+    Entity placeTagEntity = new Entity("PlaceTag",
+        KeyFactory.keyToString(placeTag.getPlace()) + KeyFactory.keyToString(placeTag.getTag()));
+    placeTagEntity.setProperty("place", placeTag.getPlace());
+    placeTagEntity.setProperty("tag", placeTag.getTag());
+
+    datastore.put(placeTagEntity);
+  }
+
+  /** Get all the PlaceTags currently in the Datastore. */
+  public static List<PlaceTag> getAll() {
+    DatastoreService datastore = Datastore.GetSingletonService();
+    List<PlaceTag> placeTags = new ArrayList<>();
+    Query query = new Query("PlaceTag").addSort("tag", SortDirection.DESCENDING).addSort("place",
+        SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+      placeTags.add(new PlaceTag(entity));
     }
+    return placeTags;
+  }
 
-
-    /**
-     * Constructs a new {@link PlaceTag} using a place and the tag
-     */
-    public PlaceTag(Place place, Tag tag) {
-        this(place.getId());
-        this(tag.getLabel());
-        // would PlaceTag(place.getId(), tag.getLabel()) work too,
-        // as in using the constructor above? 
+  /** Get all the PlaceTags who have the same tag currently in the Datastore. */
+  public static List<PlaceTag> getByTag(Tag tag) {
+    DatastoreService datastore = Datastore.GetSingletonService();
+    List<PlaceTag> placeTags = new ArrayList<>();
+    String label = tag.getLabel();
+    Query query = new Query("PlaceTag").setFilter(new Query.FilterPredicate("tag", FilterOperator.EQUAL, label))
+        .addSort("place", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+      placeTags.add(new PlaceTag(entity));
     }
+    return placeTags;
+  }
 
-    /** Return PlaceTag data using based on entity from search query. */
-    public PlaceTag(Entity entity) {
-        // create key based on the string returned from getting the place and tag
-        this.place = KeyFactory.createKey("Place", entity.getProperty("place"))
-        this.tag = KeyFactory.createKey("Tag", entity.getProperty("tag"));
-    }
+  public Key getPlace() {
+    return place;
+  }
 
-    /** Stores a new PlaceTag in Datastore. */
-    public static void store(PlaceTag placeTag) {
-        DatastoreService datastore = Datastore.GetSingletonService();
-        Entity placeTagEntity = new Entity(
-          "PlaceTag", 
-          KeyFactory.keyToString(place.getPlace()) + KeyFactory.keyToString(place.getTag())
-        );
-        placeTagEntity.setProperty("place", place.getPlace());
-        placeTagEntity.setProperty("tag", place.getTag());
-
-        datastore.put(placeEntity);
-    }
-
-    /** Get all the PlaceTags currently in the Datastore. */
-    public static List<PlaceTag> getAll() {
-      DatastoreService datastore = Datastore.GetSingletonService();
-      List<PlaceTag> placeTags = new ArrayList<>();
-      Query query = new Query("PlaceTag")
-      .addSort("tag", SortDirection.DESCENDING)
-      .addSort("place", SortDirection.DESCENDING);
-      PreparedQuery results = datastore.prepare(query);
-      for (Entity entity : results.asIterable()) {
-        placeTags.add(new Place(entity));
-      }
-      return placeTags;
-    }
-
-    /** Get all the PlaceTags who have the same tag currently in the Datastore. */
-    public static List<PlaceTag> getByTag(Tag tag) {
-      DatastoreService datastore = Datastore.GetSingletonService();
-      List<PlaceTag> placeTags = new ArrayList<>();
-      String label = tag.getLabel();
-      Query query = new Query("PlaceTag")
-      .setFilter(new Query.FilterPredicate("tag", FilterOperator.EQUAL, label))
-      .addSort("place", SortDirection.DESCENDING);
-      PreparedQuery results = datastore.prepare(query);
-      for (Entity entity : results.asIterable()) {
-        placeTags.add(new Place(entity));
-      }
-      return placeTags;
-    }
-
-    public Key getPlace() {
-        return place;
-    }
-
-    public Key getTag() {
-        return tag;
-    }
-
+  public Key getTag() {
+    return tag;
+  }
 
 }
